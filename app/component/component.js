b3342b3e313cdbb174b8677ad8f9ca9435c08edb
Shaun Husain <shaun@intellectual-tech.com>
2014-11-12 21:47:36 -0600

    Changed to having the JS create the CSS instead of trying to read it.

--- app/component/component.js
+++ app/component/component.js
@@ -7,44 +7,123 @@
   * Description
   */
 
-  var getClassStyle = function(className, requiredStyle){
-      var x, sheets,classes;
-
-
-      var requiredStyleFulfilled = true;
-      if(requiredStyle)
-        requiredStyleFulfilled = false;
-
-      for( sheets=document.styleSheets.length-1; sheets>=0; sheets-- ){
-          classes = document.styleSheets[sheets].rules || document.styleSheets[sheets].cssRules;
-          for(x=0;x<classes.length;x++) {
-              if(classes[x].selectorText.indexOf(className)!=-1){
-                  var classStyleTxt = (classes[x].cssText ? classes[x].cssText : classes[x].style.cssText).match(/\{\s*([^{}]+)\s*\}/)[1];
-                  var classStyles = {};
-                  var styleSets = classStyleTxt.match(/([^;:]+:\s*[^;:]+\s*)/g);
-                  for(var y=0;y<styleSets.length;y++){
-                      var style = styleSets[y].match(/\s*([^:;]+):\s*([^;:]+)/);
-                      if(style.length > 2)
-                      {
-                        if(style[1] == requiredStyle)
-                          requiredStyleFulfilled = true;
-                        
-                        classStyles[style[1]]=style[2];
-                      }
-                  }
-                  if(requiredStyleFulfilled)
-                    return classStyles;
-              }
-          }
-      }
-      return false;
-  };
-
-
   var mod = angular.module('componentModule', ['ui.router'])
-  .constant('EXTRA_PADDING', 3)
-      
-  .directive('holyGrail', function($window, $timeout, EXTRA_PADDING){
+  mod.factory('StyleSheetHandler', function(){
+    var StyleSheetHandler = {};
+
+    StyleSheetHandler.createStyleSheet = function(){
+      if(!document.styleSheets) {
+        return;
+      }
+
+      if(document.getElementsByTagName("head").length == 0) {
+        return;
+      }
+
+      var styleSheet;
+      var GEN_ID = "generated";
+
+      var styleSheetElement = document.createElement("style");
+      styleSheetElement.type = "text/css";
+      styleSheetElement.title = GEN_ID;
+
+      document.getElementsByTagName("head")[0].appendChild(styleSheetElement);
+
+      //Find the newly addded stylesheet object
+      //since one wasn't found in the first loop above
+      //the one we just added must be the only non disabled
+      //one (this is probably a bad assumption it should do the same checks as above for media)
+      for( var i = document.styleSheets.length-1; i>=0; i--) {
+        if(document.styleSheets[i].disabled) {
+          continue;
+        }
+        if(document.styleSheets[i].title == GEN_ID)
+        {
+          styleSheet = document.styleSheets[i];
+        }
+      }
+
+      return {mediaType:typeof styleSheet.media, styleSheet:styleSheet}
+    }
+
+    StyleSheetHandler.findEnabledScreenStylesheet = function(){
+      if(!document.styleSheets) {
+        return;
+      }
+
+      if(document.getElementsByTagName("head").length == 0) {
+        return;
+      }
+      //If there are existing stylesheets
+      if(document.styleSheets.length > 0) {
+
+        //Iterate through them
+        for( i = 0; i < document.styleSheets.length; i++) {
+          //If a stylesheet is disabled skip this iteration
+          if(document.styleSheets[i].disabled) {
+            continue;
+          }
+          //Get the media from the current stylesheet
+          var media = document.styleSheets[i].media;
+          mediaType = typeof media;
+
+          //Check the media type (must vary between browsers)
+          if(mediaType == "string") {
+            //Check that the media target, if it targets the screen use it
+            if(media == "" || (media.indexOf("screen") != -1)) {
+              styleSheet = document.styleSheets[i];
+            }
+          } else if(mediaType == "object") {
+            if(media.mediaText == "" || (media.mediaText.indexOf("screen") != -1)) {
+              styleSheet = document.styleSheets[i];
+            }
+          }
+
+          //If we have found a usable stylesheet during this iteration exit the loop
+          if( typeof styleSheet != "undefined") {
+            return styleSheet;
+          }
+        }
+      }
+    }
+
+    StyleSheetHandler.createCSSSelector = function(selector, style){
+      var styleSheet = StyleSheetHandler.baseStyleSheet.styleSheet;
+      var mediaType = StyleSheetHandler.baseStyleSheet.mediaType;
+
+      //Finally after we have the stylesheet to use we need to add the new rule appropriately
+      //this searches for an existing matching rule
+      if(mediaType == "string") {
+        // for(var i = 0; i < styleSheet.rules.length; i++) {
+        //   if(styleSheet.rules[i].selectorText && styleSheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+        //     styleSheet.rules[i].style.cssText = style;
+        //     return;
+        //   }
+        // }
+
+        //If we didn't find a matching rule we add the rule
+        styleSheet.addRule(selector, style);
+
+      } else if(mediaType == "object") {
+        // for(var i = 0; i < styleSheet.cssRules.length; i++) {
+        //   if(styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+        //     styleSheet.cssRules[i].style.cssText = style;
+        //     return;
+        //   }
+        // }
+
+        //Again but for other browsers if we didn't find the rule we add a new rule
+        styleSheet.insertRule(selector + "{" + style + "}", styleSheet.cssRules.length);
+      }
+    }
+
+    StyleSheetHandler.baseStyleSheet = StyleSheetHandler.createStyleSheet();
+
+    return StyleSheetHandler;
+
+  })
+  
+  .directive('holyGrail', function($window, $timeout, StyleSheetHandler){
     return {
       restrict:'E',
       scope:{showWest:'=',showEast:'='},
@@ -56,33 +135,60 @@
         }
       },
       link:function(scope, element, attr){
+        console.log(element[0].offsetWidth);
         var eastWidth = 0;
         var westWidth = 0;
         
-        var westClassInfo = {
-          width: getClassStyle('.west-col', 'width')['width'],
-          minWidth: getClassStyle('.west-col', 'min-width')['min-width'],
-          maxWidth: getClassStyle('.west-col', 'max-width')['max-width'],
-          padding: getClassStyle('.west-col', 'padding')['padding'],
-          left: getClassStyle('.west-col', 'left')['left']
-        };
-        
-        var eastClassInfo = {
-          width: getClassStyle('.east-col', 'width')['width'],
-          minWidth: getClassStyle('.east-col', 'min-width')['min-width'],
-          maxWidth: getClassStyle('.east-col', 'max-width')['max-width'],
-          padding: getClassStyle('.east-col', 'padding')['padding'],
-          right: getClassStyle('.east-col', 'right')['right']
-        };
+        var layoutConfig = {
+          westClassInfo: {
+            width: "20%",
+            minWidth: "60px",
+            maxWidth: "275px",
+            padding: "9px"
+          },
+          eastClassInfo:{
+            width: "20%",
+            minWidth: "60px",
+            maxWidth: "275px",
+            padding: "9px"
+          },
+          headRowHeight: "60px",
+          footRowHeight: "60px",
+          outsideBorder: "3px",
+          insideBorder: "3px",
+          insidePadding: "9px"
+        }
 
-        console.log(eastClassInfo)
+        //Some aliases
+        var cc, calculatedConfig;
+        var wci;
+        var eci;
 
-        function determinePixelSize(parentSize, classInfo){
-          var retSize = 0;
+        cc = calculatedConfig = {};
+        var lc = layoutConfig;
 
-          var classWidth = normalizePercentAndPixel(classInfo.width, parentSize);
-          var classMinWidth = normalizePercentAndPixel(classInfo.minWidth, parentSize);
-          var classMaxWidth = normalizePercentAndPixel(classInfo.maxWidth, parentSize);
+        //Function determines all the pixel sizes as Numbers
+        //based on the string input, converts each based on
+        //the percentage or the value supplied.
+        function calcPixelSizes(inputObj, resultObj){
+          _.forOwn(inputObj, function(value, key, obj){
+            if(key == "width"){
+              resultObj[key] = determinePixelSize(obj); 
+            }
+            else if(angular.isString(value)) {
+              resultObj[key] = normalizePercentAndPixel(value);
+            }
+            else if(angular.isObject(value)){
+              resultObj[key] = {};
+              calcPixelSizes(value, resultObj[key]);
+            }
+          });
+        }
+
+        function determinePixelSize(classInfo){
+          var classWidth = normalizePercentAndPixel(classInfo.width);
+          var classMinWidth = normalizePercentAndPixel(classInfo.minWidth);
+          var classMaxWidth = normalizePercentAndPixel(classInfo.maxWidth);
 
           if(classWidth < classMinWidth)
             classWidth = classMinWidth;
@@ -92,51 +198,46 @@
           return Math.ceil(classWidth);
         }
 
-        function normalizePercentAndPixel(stringValue, parentSize){
+        function normalizePercentAndPixel(stringValue){
           var parsed = parseInt(stringValue);
           if(stringValue.indexOf("%")!=-1)
-            return parsed/100*parentSize;
+            return parsed/100*element[0].offsetWidth;
           else
             return parsed;
         }
 
         function calcEastAndWest(){
-          var parElementWidth = scope.panels['eastBody'].parent()[0].clientWidth;
-          
-          westWidth = determinePixelSize(parElementWidth, westClassInfo);
-          eastWidth = determinePixelSize(parElementWidth, eastClassInfo);
-
-          console.log(westWidth,eastWidth);
+          calcPixelSizes(lc, cc);
+          wci = cc.westClassInfo;
+          eci = cc.eastClassInfo;
 
           $timeout(updateStyles);
         };
         
         function updateStyles() {
-          var parElementWidth = scope.panels['eastBody'].parent()[0].clientWidth;
-
-          var westLeft = normalizePercentAndPixel(westClassInfo.left, parElementWidth);
-          var westPadding = normalizePercentAndPixel(westClassInfo.padding, parElementWidth);
-          var eastRight = normalizePercentAndPixel(eastClassInfo.right, parElementWidth);
-          var eastPadding = normalizePercentAndPixel(eastClassInfo.padding, parElementWidth);
+          if(!wci)
+            return;
+          westWidth = wci.width;
+          eastWidth = eci.width;
           
           var newStyle = {};
           
           if(!scope.showWest) {
-            newStyle.left=westClassInfo.left;
-            scope.panels['westBody'][0].style.left = scope.panels['westHead'][0].style.left = scope.panels['westFoot'][0].style.left = -(westWidth+westPadding*2) + "px";
+            newStyle.left=cc.outsideBorder + "px";
+            scope.panels['westBody'][0].style.left = scope.panels['westHead'][0].style.left = scope.panels['westFoot'][0].style.left = -(westWidth+wci.padding*2) + "px";
           }
           else {
-            newStyle.left = (westWidth+westPadding*2+westLeft+EXTRA_PADDING)+"px";
-            scope.panels['westBody'][0].style.left = scope.panels['westHead'][0].style.left = scope.panels['westFoot'][0].style.left = westLeft + "px";
+            newStyle.left = (westWidth+cc.insideBorder+cc.outsideBorder+cc.insidePadding*2)+"px";
+            scope.panels['westBody'][0].style.left = scope.panels['westHead'][0].style.left = scope.panels['westFoot'][0].style.left = cc.outsideBorder + "px";
           }
 
           if(!scope.showEast) {
-            newStyle.right = eastClassInfo.right;
-            scope.panels['eastBody'][0].style.right = scope.panels['eastHead'][0].style.right = scope.panels['eastFoot'][0].style.right = -(eastWidth+eastPadding*2) + "px";
+            newStyle.right = cc.outsideBorder + "px";
+            scope.panels['eastBody'][0].style.right = scope.panels['eastHead'][0].style.right = scope.panels['eastFoot'][0].style.right = -(eastWidth+eci.padding*2) + "px";
           }
           else {
-            newStyle.right = (eastWidth+eastPadding*2+eastRight+EXTRA_PADDING)+"px";
-            scope.panels['eastBody'][0].style.right = scope.panels['eastHead'][0].style.right = scope.panels['eastFoot'][0].style.right = eastRight + "px";
+            newStyle.right = (eastWidth+cc.insideBorder+cc.outsideBorder+cc.insidePadding*2)+"px";
+            scope.panels['eastBody'][0].style.right = scope.panels['eastHead'][0].style.right = scope.panels['eastFoot'][0].style.right = cc.outsideBorder + "px";
           }
 
           angular.extend(scope.panels['mainBody'][0].style, newStyle);
@@ -148,7 +249,36 @@
         scope.$watch('showEast', updateStyles);
 
         angular.element($window).bind('resize', calcEastAndWest);
-        angular.element(document).ready(calcEastAndWest);
+        angular.element(document).ready(setup);
+
+        function setup(){
+          calcPixelSizes(lc, cc);
+          wci = cc.westClassInfo;
+          eci = cc.eastClassInfo;
+          console.log(cc)
+
+          StyleSheetHandler.createCSSSelector('.west-col, .main-col, .east-col', 'padding:9px;');
+          // console.log('setting cw', 'width: '+wci.width+';  min-width: '+wci.minWidth+';  max-width: '+wci.maxWidth+';')
+          
+          /****************************************************( Side Widths )*/
+          StyleSheetHandler.createCSSSelector('.west-col','width: '+wci.width+'px;  min-width: '+wci.minWidth+'px;  max-width: '+wci.maxWidth+'px;')
+          StyleSheetHandler.createCSSSelector('.east-col','width: '+eci.width+'px;  min-width: '+eci.minWidth+'px;  max-width: '+eci.maxWidth+'px;')
+          
+          /********************************************************( Heights )*/
+          StyleSheetHandler.createCSSSelector('.head-row', 'height:'+cc.headRowHeight+'px;');
+          StyleSheetHandler.createCSSSelector('.foot-row', 'height:'+cc.footRowHeight+'px;');
+
+          /*****************************************************( Positions )*/
+          // var topOffset = cc.headRowHeight
+          StyleSheetHandler.createCSSSelector('.head-row', 'position: absolute;  top: '+cc.outsideBorder+'px;');
+          StyleSheetHandler.createCSSSelector('.body-row', 'position: absolute;  top: 82px; bottom: 82px;');
+          StyleSheetHandler.createCSSSelector('.foot-row', 'position: absolute;  bottom: 3px;');
+          StyleSheetHandler.createCSSSelector('.west-col', 'position: absolute;  left: 3px;');
+          StyleSheetHandler.createCSSSelector('.east-col', 'position: absolute;  right: 3px;');
+          StyleSheetHandler.createCSSSelector('.main-col', 'z-index: 100; left:0px; right:0px');
+
+          updateStyles();
+        }
       }
     }
   })
